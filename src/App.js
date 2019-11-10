ea05047348875160fc3038bd08ccf00c3bc10cf9
Rodrigo Gualberto <itzdrigz@gmail.com>
2019-11-10 08:19:54 -0500

    Add to App to setup for simple test case

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function App () {
+  const [displayDescription, toggleDescription] = useState(false);
+
+  function handleToggleClick () {
+    toggleDescription(!displayDescription)
+  }
+
   return (
-    <div style={{"textAlign": "center"}}>
+    <div className="app">
       <h1>App loaded</h1>
+      <button onClick={handleToggleClick}>See Description</button>
+      {displayDescription &&
+        <div className="description">Here&apos;s your super slick application homie!</div>
+      }
     </div>
   );
 }
